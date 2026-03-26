66499c7727cbe3022dfbce304fba4efa93db659d
aebgbigasi <qwopop2@gmail.com>
2026-03-26 16:44:23 +0900

    asd
    asd

--- lib/ui/pages/todo/widget/filter_row.dart
+++ lib/ui/pages/todo/widget/filter_row.dart
@@ -5,6 +5,35 @@ class FilterRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      scrollDirection: .horizontal,
+      child: Row(
+        spacing: 8,
+        children: [
+          _buildFilter('전체', isSelected: true),
+          _buildFilter('업무'),
+          _buildFilter('개인'),
+          _buildFilter('개'),
+          _buildFilter('인'),
+          _buildFilter('고양이'),
+          _buildFilter('고양인'),
+          _buildFilter('고양2'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFilter(String text, {bool isSelected = false}) {
+    return Container(
+      padding: .symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.deepPurpleAccent : Colors.grey,
+        borderRadius: .all(.circular(50)),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: .w700),
+      ),
+    );
   }
 }
--- lib/ui/pages/todo_page.dart
+++ lib/ui/pages/todo_page.dart
@@ -1,6 +1,8 @@
 import 'package:example/ui/pages/todo/widget/filter_row.dart';
 import 'package:example/ui/pages/todo/widget/progress_card.dart';
+import 'package:example/ui/pages/todo/widget/todo_list.dart';
 import 'package:flutter/material.dart';
+
 import 'todo/widget/header.dart';
 
 class TodoPage extends StatelessWidget {
@@ -10,15 +12,26 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            //타이틀 상단
-            Header(),
-            ProgressCard(),
-            FilterRow(),
-          ],
+        child: Padding(
+          padding: .all(24),
+          child: Column(
+            spacing: 24,
+            crossAxisAlignment: .stretch,
+            children: [
+              //타이틀 상단
+              const Header(),
+              const ProgressCard(),
+              const FilterRow(),
+              _buildTodoTitle(),
+              const TodoList(),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget _buildTodoTitle() {
+  return Text('오늘 할 일', style: TextStyle(fontSize: 18, fontWeight: .bold));
 }
