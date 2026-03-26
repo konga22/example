c5db9a0447bf915ea5e1f1adf4e07b47fd91e3bc
aebgbigasi <qwopop2@gmail.com>
2026-03-26 14:08:15 +0900

    1
    1

--- lib/ui/pages/todo/widget/progress_card.dart
+++ lib/ui/pages/todo/widget/progress_card.dart
@@ -7,21 +7,19 @@ class ProgressCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const .all(24),
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         color: Color(0xFF8B5CF6),
         borderRadius: .all(.circular(24)),
       ),
-      child: const Column(
-        crossAxisAlignment: .start,
-        children:
-        DefaultTextStyle(
-           Row([
-          children: [
-            Text('오늘의 진행률'),
-            Text('60%')
-            )
-            ],
-        ), Text('A'), Text('A')],
+      child: Column(
+        children: [
+          DefaultTextStyle(
+            style: TextStyle(color: Colors.white),
+            child: Row(children: [Text('오늘의 진행률'), Text('60%')]),
+          ),
+          Text('A'),
+          Text('A'),
+        ],
       ),
     );
   }
